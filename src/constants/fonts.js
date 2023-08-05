e19e65f2600d34184c5a4a9b2555954018e53928
IkkiRama <rifkivamaus@gmail.com>
2023-08-05 09:55:09 +0700

    init

---
+++ src/constants/fonts.js
@@ -0,0 +1,7 @@
+// const [fontsLoaded] = useFonts({
+//   DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+//   DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+//   DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+// });
+
+// export default fontsLoaded;
